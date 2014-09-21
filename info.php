b8cc2cfcb8584cd6ace6437f23a2d0186a55d51a
jcpwfloi <jcpwfloi@gmail.com>
2014-09-21 05:40:02 +0000

    Excalibur v1.2 alpha started

---
+++ info.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<!-- 新 Bootstrap 核心 CSS 文件 -->
+<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- 可选的Bootstrap主题文件（一般不用引入） -->
+<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+
+<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+<script src="http://cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<?
+header('Content-Type: text/html; charset=utf8');
+?>
+<body>
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header" id="barhead">
+			<a class="navbar-brand" href="index.php">主站</a>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav" id="leftbar">
+				<li>
+					<a href="#">基本信息</a>
+				</li>
+				<li>
+					<a href="#">课时情况</a>
+				</li>
+				<li>
+					<a href="#">服务队管理</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+</body>
+
